c9efe8e827dcf39d0d5e526726dcc07f7390fcaf
Roman Timashev <roman@tmshv.ru>
2026-04-18 13:24:08 +0300

    Convert app-content to light DOM

--- src/components/app-content.ts
+++ src/components/app-content.ts
@@ -1,23 +1,5 @@
-import { h, style } from '../lib/dom'
+import './app-content.css'
 
-const CSS = `
-  :host {
-    display: block;
-    height: 100%;
-    overflow-y: auto;
-    overscroll-behavior-y: contain;
-    -webkit-overflow-scrolling: touch;
-    padding-top: calc(env(safe-area-inset-top) + 80px);
-    padding-bottom: calc(env(safe-area-inset-bottom) + 80px);
-  }
-`
-
-class AppContent extends HTMLElement {
-  constructor() {
-    super()
-    const shadow = this.attachShadow({ mode: 'open' })
-    shadow.append(style(CSS), h('slot'))
-  }
-}
+class AppContent extends HTMLElement {}
 
 customElements.define('app-content', AppContent)
